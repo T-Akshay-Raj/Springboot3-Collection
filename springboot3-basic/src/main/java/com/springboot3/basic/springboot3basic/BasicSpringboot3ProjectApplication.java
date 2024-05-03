943eead43fa98c31bf03c6828c67f1ba273d4e88
T-Akshay-Raj <161284052+T-Akshay-Raj@users.noreply.github.com>
2024-05-03 08:34:07 +0530

    Basic Springboot template project
    Java Version: 17 and Springboot Version: 3

---
+++ springboot3-basic/src/main/java/com/springboot3/basic/springboot3basic/BasicSpringboot3ProjectApplication.java
@@ -0,0 +1,14 @@
+package com.springboot3.basic.springboot3basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringboot3ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringboot3ProjectApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ springboot3-basic/src/test/java/com/springboot3/basic/springboot3basic/BasicSpringboot3ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot3.basic.springboot3basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringboot3ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
