edb3ded48586e6d3eeb15e30e1f3148e9ddb6f6e
T-Akshay-Raj <161284052+T-Akshay-Raj@users.noreply.github.com>
2024-05-03 08:37:34 +0530

    Basic Springboot Web Project
    Version Used:
    Springboot: 3
    Java : 17

---
+++ springboot3-web-basic/src/main/java/com/springboot3/webbasic/Springboot3WebBasicApplication.java
@@ -0,0 +1,13 @@
+package com.springboot3.webbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3WebBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3WebBasicApplication.class, args);
+	}
+
+}
---
+++ springboot3-web-basic/src/test/java/com/springboot3/webbasic/Springboot3WebBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot3.webbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3WebBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
